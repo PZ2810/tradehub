c73af464397fd0be8556ef05c8511752cb8dbdc0
Pranav Zhawar <pranavzhawar@gmail.com>
2026-02-17 18:01:13 +0530

    added bootstrap,font-awesome,media

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 function Hero() {
-    return (  <h1>Hero</h1> );
+    return (  <h1>Hero <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+<span class="sr-only">Loading...</span></h1> );
 }
 
 export default Hero;
