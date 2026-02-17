6faac6614503549b24793f505657c17de88d06de
Pranav Zhawar <pranavzhawar@gmail.com>
2026-02-17 18:55:21 +0530

    Home-hero section

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 function Hero() {
-    return (  <h1>Hero <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
-<span class="sr-only">Loading...</span></h1> );
+    return (  <div className='container p-5'>
+        <div className='row text-center'>
+
+            <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'  />
+
+            <h1 className='mt-3 mb-2'>Invest in everything</h1>
+            <p className='mb-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            <button className='p-2 btn btn-primary fs-5' style={{width:"18%", margin:"auto"}}>Sign up for free</button>
+
+            
+
+            </div>
+
+    </div>
+     );
 }
 
 export default Hero;
