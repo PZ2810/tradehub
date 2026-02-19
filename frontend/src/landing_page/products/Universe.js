f0f89511087b94bbd63fced51c46b49704fa4964
Pranav Zhawar <pranavzhawar@gmail.com>
2026-02-20 00:18:06 +0530

    updated Products

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -5,35 +5,36 @@ function Universe() {
   return (
     <div className="container mt-5">
       <div className="row text-center">
-        <h1>The Zerodha Universe</h1>
+        <h1 className="fs-4 mb-4">The Zerodha Universe </h1>
         <p>
           Extend your trading and investment experience even further with our
           partner platforms
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
+          <img src="media/images/zerodhaFundhouse.png"className="w-50" 
+    alt="Zerodha Fundhouse" />
+          <p className="text-small text-muted">Our asset management venture</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/sensibullLogo.svg" className="w-50" alt="Sensibull Logo" />
+          <p className="text-small text-muted">Options trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/tijori.svg" className="w-50" alt="Tijori Logo" />
+          <p className="text-small text-muted"> Investment research platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/streakLogo.png" className="w-50" alt="Streak Logo" />
+          <p className="text-small text-muted">Systematic trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" className="w-50" alt="Smallcase Logo" />
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/dittoLogo.png" className="w-50" alt="Ditto Logo"  />
+          <p className="text-small text-muted">Personalised advice on life and health insurance</p>
         </div>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
